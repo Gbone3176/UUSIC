0d22ec1ea2c49d8f29f3c8971e8c0a2616fa375a
Gbone0790 <21307130160@m.fudan.edu.cn>
2025-08-01 07:22:40 +0000

    Changes to be committed: 	modified:   baseline/PG.sh 	new file:   baseline/exp_docs/debug.md 	new file:   baseline/exp_docs/result.md 	new file:   baseline/exp_docs/sota.md

--- baseline/PG.sh
+++ baseline/PG.sh
@@ -11,18 +11,29 @@ export CUDA_VISIBLE_DEVICES=0
 #     --max_epochs=200 \
 #     --root_path=/cpfs01/projects-HDD/cfff-906dc71fafda_HDD/gbw_21307130160/challenge-main/baseline/data/ \
     
-######## test PG ########
+# ######## test PG ########
+# torchrun \
+#     --nproc_per_node=1 \
+#     --master_port=2345 \
+#     omni_test_PG_decoders.py \
+#     --root_path ./data \
+#     --output_dir exp_out/trail_debug_17—294epoch \
+#     --resume /cpfs01/projects-HDD/cfff-906dc71fafda_HDD/gbw_21307130160/challenge-main/baseline/exp_out/trail_debug_16/best_model_294_0.4725.pth \
+#     --batch_size 1 \
+#     --img_size 224 \
+#     --cfg configs/swin_tiny_patch4_window7_224_lite-PG.yaml \
+#     --prompt \
+#     --is_saveout \
+
+
+######## train PG(Frozen Encoder) ########
 torchrun \
     --nproc_per_node=1 \
-    --master_port=2345 \
-    omni_test_PG_decoders.py \
-    --root_path ./data \
-    --output_dir exp_out/trail_debug_17—294epoch \
-    --resume /cpfs01/projects-HDD/cfff-906dc71fafda_HDD/gbw_21307130160/challenge-main/baseline/exp_out/trail_debug_16/best_model_294_0.4725.pth \
-    --batch_size 1 \
-    --img_size 224 \
-    --cfg configs/swin_tiny_patch4_window7_224_lite-PG.yaml \
+    --master_port=12345 \
+    omni_train_PG_decoders.py \
+    --output_dir=exp_out/trail_debug_19 \
     --prompt \
-    --is_saveout \
-
-
+    --base_lr=0.001 \
+    --batch_size=1 \
+    --max_epochs=200 \
+    --root_path=/cpfs01/project
